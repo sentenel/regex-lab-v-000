63e6a5ce4a2f511db7d643e0c76080c3deb2ad5e
Seth Wax <sethlwax@gmail.com>
2018-03-31 15:00:27 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -12,9 +12,9 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(/^[A-Z].*[!?.,;:]$/) ? true : false
+  text.match?(/^[A-Z].*[!?.,;:]$/)
 end
 
 def valid_phone_number?(phone)
-  phone.match()
+  phone.match?(/^\(?\d{3}\)?[.- ]?\d{3}[.- ]?\d{4}$/)
 end
