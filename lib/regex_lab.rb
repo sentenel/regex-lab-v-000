7092b93833631d29c46ac19258bd27bf440c489a
Seth Wax <sethlwax@gmail.com>
2018-03-31 15:22:46 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,11 +1,5 @@
-class String
-  def match?(regex)
-    self.match(regex) ? true : false
-  end
-end
-
 def starts_with_a_vowel?(word)
-  word.match?(/^[AEIOUaeiou]/)
+  word.match(/^[AEIOUaeiou]/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -17,9 +11,9 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match?(/^[A-Z].*[!?.,;:]$/)
+  text.match(/^[A-Z].*[!?.,;:]$/) ? true : false
 end
 
 def valid_phone_number?(phone)
-  phone.match?(/^\(?\d{3}\)?[.\- ]?\d{3}[.\- ]?\d{4}$/)
+  phone.match(/^\(?\d{3}\)?[.\- ]?\d{3}[.\- ]?\d{4}$/) ? true : false
 end
